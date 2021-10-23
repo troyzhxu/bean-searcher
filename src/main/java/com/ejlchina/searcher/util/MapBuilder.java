611f8e1106f9f0b84e8355cd573dd131eef7b5a8
周旭 <15735559897@qq.com>
2021-10-23 14:06:16 +0800

    新增 MapBuilder, 可以支持使用 lambda 方式添加参数

---
+++ src/main/java/com/ejlchina/searcher/util/MapBuilder.java
@@ -0,0 +1,88 @@
+package com.ejlchina.searcher.util;
+
+import com.ejlchina.searcher.param.Operator;
+
+import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.function.Function;
+
+public class MapBuilder {
+
+    /**
+     * 参数名分割符
+     */
+    private static String paramSeparator = "-";
+
+    /**
+     * 过滤运算符参数名后缀
+     */
+    private static String operationSuffix = "op";
+
+    private final Map<String, Object> map;
+
+    public MapBuilder(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public static void init(String paramSeparator, String operationSuffix) {
+        MapBuilder.paramSeparator = paramSeparator;
+        MapBuilder.operationSuffix = operationSuffix;
+    }
+
+    @FunctionalInterface
+    public interface FieldFunction<T, R> extends Function<T, R>, Serializable {  }
+
+    public MapBuilder put(String key, Object value) {
+        map.put(key, value);
+        return this;
+    }
+
+    public <T> MapBuilder put(FieldFunction<T, ?> fieldFn, Object value) {
+        String fieldName = toFieldName(fieldFn);
+        map.put(fieldName, value);
+        return this;
+    }
+
+    public <T> MapBuilder put(FieldFunction<T, ?> fieldFn, int index, Object value) {
+        String fieldName = toFieldName(fieldFn);
+        map.put(fieldName + paramSeparator + index, value);
+        return this;
+    }
+
+    public <T> MapBuilder put(FieldFunction<T, ?> fieldFn, Operator operator) {
+        String fieldName = toFieldName(fieldFn);
+        map.put(fieldName + paramSeparator + operationSuffix, operator);
+        return this;
+    }
+
+    private String toFieldName(FieldFunction<?, ?> fieldFn) {
+        try {
+            Method wrMethod = fieldFn.getClass().getDeclaredMethod("writeReplace");
+            boolean isInaccessible = !wrMethod.isAccessible();
+            if (isInaccessible) {
+                wrMethod.setAccessible(true);
+            }
+            SerializedLambda sLambda = (SerializedLambda) wrMethod.invoke(fieldFn);
+            if (isInaccessible) {
+                wrMethod.setAccessible(false);
+            }
+            String methodName = sLambda.getImplMethodName();
+            if (methodName.startsWith("get") && methodName.length() > 3) {
+                return StringUtils.firstCharToLoweCase(methodName.substring(3));
+            }
+            if (methodName.startsWith("is") && methodName.length() > 2) {
+                return StringUtils.firstCharToLoweCase(methodName.substring(2));
+            }
+            throw new IllegalStateException("can not convert method [" + methodName + "] to field name");
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException("无法反射出字段名", e);
+        }
+    }
+
+    public Map<String, Object> build() {
+        return map;
+    }
+
+}
--- src/main/java/com/ejlchina/searcher/util/MapUtils.java
+++ src/main/java/com/ejlchina/searcher/util/MapUtils.java
@@ -6,7 +6,9 @@ import java.util.Map.Entry;
 
 public class MapUtils {
 
-	
+	/**
+	 * 将一个 value 为 数组的 Map 对象，拉平为 value 为 单值的 Map 对象
+	 */
 	public static Map<String, Object> flat(Map<String, String[]> map) {
 		Map<String, Object> newMap = new HashMap<>();
 		for (Entry<String, String[]> entry: map.entrySet()) {
@@ -17,5 +19,26 @@ public class MapUtils {
 		}
 		return newMap;
 	}
-	
+
+	/**
+	 * 返回一个 lambda Map 构造器
+	 */
+	public static MapBuilder builder() {
+		return builder(new HashMap<>());
+	}
+
+	/**
+	 * 返回一个 lambda Map 构造器
+	 */
+	public static MapBuilder flatBuilder(Map<String, String[]> map) {
+		return new MapBuilder(flat(map));
+	}
+
+	/**
+	 * 返回一个 lambda Map 构造器
+	 */
+	public static MapBuilder builder(Map<String, Object> map) {
+		return new MapBuilder(map);
+	}
+
 }
