0b797b4fcf7679715c40f6aa39658198d9805f95
周旭 <15735559897@qq.com>
2021-10-30 21:42:10 +0800

    javadoc

--- bean-searcher/src/main/java/com/ejlchina/searcher/SqlExecutor.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/SqlExecutor.java
@@ -3,13 +3,11 @@ package com.ejlchina.searcher;
 /***
  * @author Troy.Zhou @ 2017-03-20
  * 
- * 检索 SQL 执行接口
- * 
+ * SQL 执行器
  * */
 public interface SqlExecutor {
 
 	/**
-	 * 
 	 * @param searchSql 检索 SQL
 	 * @return 执行结果
 	 */
--- bean-searcher/src/main/java/com/ejlchina/searcher/SqlResolver.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/SqlResolver.java
@@ -3,13 +3,16 @@ package com.ejlchina.searcher;
 /***
  * @author Troy.Zhou @ 2017-03-20
  * 
- * 检索SQL解析接口
- * 
+ * SQL 解析器
  * */
 public interface SqlResolver {
 
-
+	/**
+	 * @param metadata 元信息
+	 * @param searchParam 检索参数
+	 * @param <T> 泛型
+	 * @return 检索 SQL
+	 */
 	<T> SearchSql<T> resolve(Metadata<T> metadata, SearchParam searchParam);
-	
 	
 }
