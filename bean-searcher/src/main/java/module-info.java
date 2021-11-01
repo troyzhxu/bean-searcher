da8ccff6f776084287e7c58b3f14a4ed19d0a929
zhouxu <zhou.xu@ejlchina.com>
2021-11-01 18:25:48 +0800

    v3.0 重构：兼容 JDK9 的模块引入机制

---
+++ bean-searcher/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module bean.searcher {
+    requires java.sql;
+    requires org.slf4j;
+    exports com.ejlchina.searcher;
+    exports com.ejlchina.searcher.bean;
+    exports com.ejlchina.searcher.dialect;
+    exports com.ejlchina.searcher.implement;
+    exports com.ejlchina.searcher.param;
+    exports com.ejlchina.searcher.util;
+}
