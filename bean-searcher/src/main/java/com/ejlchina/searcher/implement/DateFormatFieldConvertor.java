3c42da1feffb3f60355efa42f16032378846b0f6
zhouxu <zhou.xu@ejlchina.com>
2021-11-03 18:52:38 +0800

    v3.0 javadoc

--- bean-searcher/src/main/java/com/ejlchina/searcher/implement/DateFormatFieldConvertor.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/implement/DateFormatFieldConvertor.java
@@ -25,6 +25,20 @@ public class DateFormatFieldConvertor implements FieldConvertor {
 
     private final ThreadLocal<Map<String, DateFormat>> local = new ThreadLocal<>();
 
+    /**
+     * 添加一个日期格式，例如：
+     * addPattern("com.example.bean.User.dateCreated", "yyyy-MM-dd");   // 这个日期格式只对 com.example.bean.User 的 dateCreated 字段起作用，优先级最高
+     * addPattern("com.example.bean.User", "yyyy-MM-dd HH");            // 这个日期格式可对 com.example.bean.User 类的所有字段起作用，但优先级比上一个低一点
+     * addPattern("com.example.bean", "yyyy-MM-dd HH:mm");              // 这个日期格式可对 com.example.bean 包下的所有类字段起作用，优先级比上一个再低一点
+     * addPattern("com.example", "yyyy-MM-dd HH:mm:ss");                // 这个日期格式可对 com.example 包下的所有类字段起作用，优先级比上一个更低一点
+     * addPattern("com", "yyyy-MM-dd HH:mm:ss:SSS");                    // 这个日期格式可对 com 包下的所有类字段起作用，但使用优先级最低
+     * @param scope 生效范围（越精确，优先级越高）
+     * @param pattern 日期格式
+     */
+    public void addPattern(String scope, String pattern) {
+        patternMap.put(scope, pattern);
+    }
+
     @Override
     public boolean supports(FieldMeta meta, Class<?> valueType, Class<?> targetType) {
         return Date.class.isAssignableFrom(valueType) && targetType == null;
@@ -87,10 +101,6 @@ public class DateFormatFieldConvertor implements FieldConvertor {
 
     public void setPatternMap(Map<String, String> patternMap) {
         this.patternMap = Objects.requireNonNull(patternMap);
-    }
-
-    public void addPattern(String key, String pattern) {
-        patternMap.put(key, pattern);
     }
 
 }
