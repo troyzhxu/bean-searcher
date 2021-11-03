6633235548d37b31a800ebbe00ec23b9dce3357d
zhouxu <zhou.xu@ejlchina.com>
2021-11-03 13:25:02 +0800

    v3.0 重构：新增 SqlInterceptor

---
+++ bean-searcher/src/main/java/com/ejlchina/searcher/SqlInterceptor.java
@@ -0,0 +1,21 @@
+package com.ejlchina.searcher;
+
+import java.util.Map;
+
+/**
+ * Sql 拦截器
+ * @author Troy.Zhou @ 2021-11-03
+ * @since v3.0.0
+ */
+public interface SqlInterceptor {
+
+    /**
+     * Sql 拦截
+     * @param searchSql 检索 SQL 信息
+     * @param paraMap 原始检索参数
+     * @param <T> 泛型
+     * @return 新的检索 SQL
+     */
+    <T> SearchSql<T> intercept(SearchSql<T> searchSql, Map<String, Object> paraMap);
+
+}
