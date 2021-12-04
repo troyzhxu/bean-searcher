8aab04edc31a12fbd1e24798fc83b40c4a151283
周旭 <15735559897@qq.com>
2021-12-04 16:15:28 +0800

    demo sql data

--- bean-searcher-demos/spring-boot-demo/src/main/resources/db/schema.sql
+++ bean-searcher-demos/spring-boot-demo/src/main/resources/db/schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE `employee` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `age` int(3) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
   `entry_date` datetime DEFAULT NULL,
   `department_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
