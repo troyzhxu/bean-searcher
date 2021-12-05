2073cac3f9bf1f83219f2ec079b8e72c371f774f
周旭 <15735559897@qq.com>
2021-12-05 22:56:24 +0800

    update demo

---
+++ bean-searcher-demos/spring-boot-demo/src/main/java/com/example/sbean/BaseBean.java
@@ -0,0 +1,11 @@
+package com.example.sbean;
+
+public class BaseBean {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+}
--- bean-searcher-demos/spring-boot-demo/src/main/java/com/example/sbean/Employee.java
+++ bean-searcher-demos/spring-boot-demo/src/main/java/com/example/sbean/Employee.java
@@ -1,12 +1,10 @@
 package com.example.sbean;
 
 import com.ejlchina.searcher.bean.*;
-import com.ejlchina.searcher.param.Operator;
 import com.example.enums.Gender;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Map;
 
 @SearchBean(
@@ -14,12 +12,9 @@ import java.util.Map;
 		joinCond = "e.department_id = d.id",
 		autoMapTo = "e"							// 字段没使用 DbField 注解时，自动映射到 employee 表
 )
-public class Employee
+public class Employee extends BaseBean			// v3.2 开始支持实体类继承 与 省略 setter 方法
 		implements BeanAware, ParamAware    	// 这两接口 都是可选的
 {
-
-	// 自动映射到 "e.id"
-	private Long id;
 
 	@DbField
 	private String name;
@@ -42,52 +37,24 @@ public class Employee
 	@DbIgnore
 	private String ignoreField = "ignore";
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public Integer getAge() {
 		return age;
 	}
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
 	public Gender getGender() {
 		return gender;
-	}
-
-	public void setGender(Gender gender) {
-		this.gender = gender;
 	}
 
 	public String getDepartment() {
 		return department;
 	}
 
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
 	public LocalDateTime getEntryDate() {
 		return entryDate;
-	}
-
-	public void setEntryDate(LocalDateTime entryDate) {
-		this.entryDate = entryDate;
 	}
 
 	public String getIgnoreField() {
