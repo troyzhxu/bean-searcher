153fcca3738585257953c36d0326b0d94092d8ce
zhouxu <zhou.xu@ejlchina.com>
2022-01-20 21:37:52 +0800

    优化 Operator 常量，使其可以直接作为 DbField.onlyOn 的值

--- bean-searcher/src/main/java/com/ejlchina/searcher/param/Operator.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/param/Operator.java
@@ -13,86 +13,86 @@ public class Operator {
 	/**
 	 * 等于
 	 */
-	public static final Equal Equal = new Equal();
+	public static final Class<Equal> Equal = Equal.class;
 
 	/**
 	 * 不等于
 	 */
-	public static final NotEqual NotEqual = new NotEqual();
+	public static final Class<NotEqual> NotEqual = NotEqual.class;
 
 	/**
 	 * 大于等于
 	 */
-	public static final GreaterEqual GreaterEqual = new GreaterEqual();
+	public static final Class<GreaterEqual> GreaterEqual = GreaterEqual.class;
 	
 	/**
 	 * 大于
 	 */
-	public static final GreaterThan GreaterThan = new GreaterThan();
+	public static final Class<GreaterThan> GreaterThan = GreaterThan.class;
 	
 	/**
 	 * 小于等于
 	 */
-	public static final LessEqual LessEqual = new LessEqual();
+	public static final Class<LessEqual> LessEqual = LessEqual.class;
 	
 	/**
 	 * 小于
 	 */
-	public static final LessThan LessThan = new LessThan();
+	public static final Class<LessThan> LessThan = LessThan.class;
 
 	/**
 	 * 为 null
 	 */
-	public static final IsNull IsNull = new IsNull();
+	public static final Class<IsNull> IsNull = IsNull.class;
 
 	/**
 	 * 不为 null
 	 */
-	public static final NotNull NotNull = new NotNull();
+	public static final Class<NotNull> NotNull = NotNull.class;
 
 	/**
 	 * 为空
 	 */
-	public static final Empty Empty = new Empty();
+	public static final Class<Empty> Empty = Empty.class;
 	
 	/**
 	 * 不为空
 	 */
-	public static final NotEmpty NotEmpty = new NotEmpty();
+	public static final Class<NotEmpty> NotEmpty = NotEmpty.class;
 
 	/**
 	 * 包含
 	 * like '%xxx%'
 	 */
-	public static final Contain Contain = new Contain();
+	public static final Class<Contain> Contain = Contain.class;
 
 	/**
 	 * 以 .. 开始
 	 * like 'xxx%'
 	 */
-	public static final StartWith StartWith = new StartWith();
+	public static final Class<StartWith> StartWith = StartWith.class;
 	
 	/**
 	 * 以 .. 结束
 	 * like '%xxx'
 	 */
-	public static final EndWith EndWith = new EndWith();
+	public static final Class<EndWith> EndWith = EndWith.class;
 	
 	/**
 	 * 在 .. 和 .. 之间
 	 */
-	public static final Between Between = new Between();
+	public static final Class<Between> Between = Between.class;
 
 	/**
 	 * 不在 .. 和 .. 之间
 	 */
-	public static final NotBetween NotBetween = new NotBetween();
+	public static final Class<NotBetween> NotBetween = NotBetween.class;
 
 	/**
 	 * 在列表中
 	 * in (...)
 	 */
-	public static final InList InList = new InList();
+	public static final Class<InList> InList = InList.class;
 
 	/**
 	 * 多值
@@ -100,13 +100,13 @@ public class Operator {
 	 * @see #InList
 	 */
 	@Deprecated
-	public static final InList MultiValue = InList;
+	public static final Class<InList> MultiValue = InList;
 
 	/**
 	 * 不在某个集合内
 	 * not in
 	 * @since v3.3
 	 */
-	public static final NotIn NotIn = new NotIn();
+	public static final Class<NotIn> NotIn = NotIn.class;
 
 }
