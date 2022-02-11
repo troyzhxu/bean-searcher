c0001e4d9e2b82098d03cf068e5ece852c57ba79
zhouxu <zhou.xu@ejlchina.com>
2022-02-11 12:09:56 +0800

    fix DateFieldConvertor 无法将 java.sql.Date 转换为 LocalDate / LocalDateTime 的问题 与 转换 LocalDate / LocalDateTime 时会产生时区偏差的问题

--- bean-searcher/src/main/java/com/ejlchina/searcher/implement/DateFieldConvertor.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/implement/DateFieldConvertor.java
@@ -49,10 +49,22 @@ public class DateFieldConvertor implements FieldConvertor.BFieldConvertor {
                 return new Timestamp(date.getTime());
             }
             if (targetType == LocalDateTime.class) {
+                // 注意：java.sql.Date 的 toInstant() 方法会抛异常
+                if (date instanceof java.sql.Date) {
+                    LocalDate localDate = ((java.sql.Date) date).toLocalDate();
+                    return LocalDateTime.of(localDate, LocalTime.of(0, 0, 0, 0));
+                }
                 return LocalDateTime.ofInstant(date.toInstant(), zoneId);
             }
             if (targetType == LocalDate.class) {
+                // 注意：java.sql.Date 的 toInstant() 方法会抛异常
+                if (date instanceof java.sql.Date) {
+                    return ((java.sql.Date) date).toLocalDate();
+                }
                 return LocalDate.ofInstant(date.toInstant(), zoneId);
+            }
+            if (targetType == Date.class) {
+                return date;
             }
         }
         LocalDateTime dateTime;
@@ -64,7 +76,7 @@ public class DateFieldConvertor implements FieldConvertor.BFieldConvertor {
         if (targetType == LocalDateTime.class) {
             return dateTime;
         }
-        Instant instant = dateTime.toInstant(ZoneOffset.UTC);
+        Instant instant = dateTime.atZone(zoneId).toInstant();
         if (targetType == Date.class) {
             return new Date(instant.toEpochMilli());
         }
@@ -79,6 +91,7 @@ public class DateFieldConvertor implements FieldConvertor.BFieldConvertor {
         }
         throw new UnsupportedOperationException();
     }
+
 
     public ZoneId getZoneId() {
         return zoneId;
