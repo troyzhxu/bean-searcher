5f3561f074b6a5decc4246d725bac49e8a80b650
zhouxu <zhou.xu@ejlchina.com>
2022-05-09 17:09:16 +0800

    PageResolverTests

---
+++ bean-searcher/src/test/java/com/ejlchina/searcher/PageResolverTests.java
@@ -0,0 +1,99 @@
+package com.ejlchina.searcher;
+
+import com.ejlchina.searcher.implement.BasePageExtractor;
+import com.ejlchina.searcher.implement.DefaultParamResolver;
+import com.ejlchina.searcher.implement.PageOffsetExtractor;
+import com.ejlchina.searcher.implement.PageSizeExtractor;
+import com.ejlchina.searcher.param.FetchType;
+import com.ejlchina.searcher.param.Paging;
+import com.ejlchina.searcher.util.MapUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class PageResolverTests {
+
+    @Test
+    public void test_page_01() {
+        BasePageExtractor pageExtractor = new PageSizeExtractor();
+        DefaultParamResolver paramResolver = new DefaultParamResolver();
+        paramResolver.setPageExtractor(pageExtractor);
+        Paging paging = paramResolver.resolvePaging(new FetchType(FetchType.ALL), new HashMap<>());
+        Assert.assertEquals(pageExtractor.getStart(), paging.getOffset());
+        Assert.assertEquals(pageExtractor.getDefaultSize(), paging.getSize());
+    }
+
+    @Test
+    public void test_page_02() {
+        PageOffsetExtractor pageExtractor = new PageOffsetExtractor();
+        DefaultParamResolver paramResolver = new DefaultParamResolver();
+        paramResolver.setPageExtractor(pageExtractor);
+        Paging paging = paramResolver.resolvePaging(new FetchType(FetchType.ALL), new HashMap<>());
+        Assert.assertEquals(pageExtractor.getStart(), paging.getOffset());
+        Assert.assertEquals(pageExtractor.getDefaultSize(), paging.getSize());
+    }
+
+    @Test
+    public void test_page_03() {
+        BasePageExtractor pageExtractor = new PageSizeExtractor();
+        DefaultParamResolver paramResolver = new DefaultParamResolver();
+        paramResolver.setPageExtractor(pageExtractor);
+        Paging paging = paramResolver.resolvePaging(
+                new FetchType(FetchType.ALL),
+                MapUtils.builder()
+                        .page(0, pageExtractor.getMaxAllowedSize() + 10000)
+                        .build()
+        );
+        Assert.assertEquals(pageExtractor.getStart(), paging.getOffset());
+        Assert.assertEquals(pageExtractor.getMaxAllowedSize(), paging.getSize());
+    }
+
+    @Test
+    public void test_page_04() {
+        PageOffsetExtractor pageExtractor = new PageOffsetExtractor();
+        DefaultParamResolver paramResolver = new DefaultParamResolver();
+        paramResolver.setPageExtractor(pageExtractor);
+        Paging paging = paramResolver.resolvePaging(
+                new FetchType(FetchType.ALL),
+                MapUtils.builder()
+                        .page(0, pageExtractor.getMaxAllowedSize() + 10000)
+                        .build()
+        );
+        Assert.assertEquals(pageExtractor.getStart(), paging.getOffset());
+        Assert.assertEquals(pageExtractor.getMaxAllowedSize(), paging.getSize());
+    }
+
+    @Test
+    public void test_page_05() {
+        PageSizeExtractor pageExtractor = new PageSizeExtractor();
+        DefaultParamResolver paramResolver = new DefaultParamResolver();
+        paramResolver.setPageExtractor(pageExtractor);
+        Paging paging = paramResolver.resolvePaging(
+                new FetchType(FetchType.ALL),
+                MapUtils.builder()
+                        .put("page", 0)
+                        .put("size", pageExtractor.getMaxAllowedSize() + 10000)
+                        .build()
+        );
+        Assert.assertEquals(pageExtractor.getStart(), paging.getOffset());
+        Assert.assertEquals(pageExtractor.getMaxAllowedSize(), paging.getSize());
+    }
+
+    @Test
+    public void test_page_06() {
+        PageOffsetExtractor pageExtractor = new PageOffsetExtractor();
+        DefaultParamResolver paramResolver = new DefaultParamResolver();
+        paramResolver.setPageExtractor(pageExtractor);
+        Paging paging = paramResolver.resolvePaging(
+                new FetchType(FetchType.ALL),
+                MapUtils.builder()
+                        .put("page", 0)
+                        .put("size", pageExtractor.getMaxAllowedSize() + 10000)
+                        .build()
+        );
+        Assert.assertEquals(pageExtractor.getStart(), paging.getOffset());
+        Assert.assertEquals(pageExtractor.getMaxAllowedSize(), paging.getSize());
+    }
+
+}
