1e744295bf1f51b244a124dfc2b26ca693b363f7
zhouxu <zhou.xu@ejlchina.com>
2022-05-10 23:07:25 +0800

    与 MyBatis Plus 性能对比

--- performance/vs-mybatis-plus/src/test/java/com/example/demo/DemoApplicationTests.java
+++ performance/vs-mybatis-plus/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.ejlchina.searcher.BeanSearcher;
 import com.example.demo.entity.Employee;
 import com.example.demo.mapper.EmployeeMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,21 +20,45 @@ class DemoApplicationTests {
 	private EmployeeMapper employeeMapper;
 
 	@Test
-	void testBeanSearcher() {
-		long t0 = System.nanoTime();
-		List<Employee> list = beanSearcher.searchList(Employee.class, null);
-		long cost = System.nanoTime() - t0;
-		System.out.println("查出数量："+ list.size());
-		System.out.println("BeanSearcher 耗时：" + cost);
+	void warmup() {
+		System.out.println("热身...");
+		long t0 = System.currentTimeMillis();
+		for (int i = 0; i < 1000; i++) {
+			employeeMapper.selectList(null);
+			beanSearcher.searchAll(Employee.class, null);
+		}
+		long cost = System.currentTimeMillis() - t0;
+		System.out.println("热身完毕，耗时：" + cost + "ms");
 	}
 
 	@Test
-	void testEmployeeMapper() {
-		long t0 = System.nanoTime();
-		List<Employee> list = employeeMapper.selectList(null);
-		long cost = System.nanoTime() - t0;
-		System.out.println("查出数量："+ list.size());
-		System.out.println("MyBatisPlus 耗时：" + cost);
+	void testBeanSearcher() {
+		System.out.println("BeanSearcher 执行 1000 次...");
+		long t0 = System.currentTimeMillis();
+		int totalCount = 0;
+		for (int i = 0; i < 1000; i++) {
+			List<Employee> list = beanSearcher.searchAll(Employee.class, null);
+			totalCount += list.size();
+		}
+		long cost = System.currentTimeMillis() - t0;
+		System.out.println("累计 Select 数量：" + totalCount);
+		System.out.println("BeanSearcher 耗时：" + cost + "ms");
 	}
+
+	@Test
+	void testMyBatisPlus() {
+		System.out.println("MyBatisPlus 执行 1000 次 ...");
+		long t0 = System.currentTimeMillis();
+		int totalCount = 0;
+		for (int i = 0; i < 1000; i++) {
+			List<Employee> list = employeeMapper.selectList(null);
+			totalCount += list.size();
+		}
+		long cost = System.currentTimeMillis() - t0;
+		System.out.println("累计 Select 数量：" + totalCount);
+		System.out.println("MyBatisPlus 耗时：" + cost + "ms");
+	}
+
+
 
 }
