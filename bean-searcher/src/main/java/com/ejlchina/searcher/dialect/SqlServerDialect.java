b5e955be7316fba112fb5dd878448934b8d067aa
周旭 <15735559897@qq.com>
2022-05-22 23:12:15 +0800

    SqlServerDialect

---
+++ bean-searcher/src/main/java/com/ejlchina/searcher/dialect/SqlServerDialect.java
@@ -0,0 +1,33 @@
+package com.ejlchina.searcher.dialect;
+
+
+import com.ejlchina.searcher.SqlWrapper;
+import com.ejlchina.searcher.param.Paging;
+
+/**
+ * SqlServer (v2012+) 方言实现
+ * @author Troy.Zhou
+ * @since v3.7.0
+ * */
+public class SqlServerDialect implements Dialect {
+
+	@Override
+	public void toUpperCase(StringBuilder builder, String dbField) {
+		builder.append("upper").append("(").append(dbField).append(")");
+	}
+	
+	@Override
+	public SqlWrapper<Object> forPaginate(String fieldSelectSql, String fromWhereSql, Paging paging) {
+		SqlWrapper<Object> wrapper = new SqlWrapper<>();
+		StringBuilder ret = new StringBuilder();
+		ret.append(fieldSelectSql).append(fromWhereSql);
+		if (paging != null) {
+			ret.append(" offset ? rows fetch next ? rows only");
+			wrapper.addPara(paging.getOffset());
+			wrapper.addPara(paging.getSize());
+		}
+		wrapper.setSql(ret.toString());
+		return wrapper;
+	}
+
+}
