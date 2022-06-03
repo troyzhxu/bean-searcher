e0fedab7d98542f65a78d51b43bcb6b88691911d
周旭 <15735559897@qq.com>
2022-06-03 22:51:53 +0800

    springboot demo

--- bean-searcher-demos/spring-boot-demo/src/main/java/com/example/Application.java
+++ bean-searcher-demos/spring-boot-demo/src/main/java/com/example/Application.java
@@ -1,9 +1,7 @@
 package com.example;
 
-import com.example.operator.MyOp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,12 +17,6 @@ public class Application implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
-    }
-
-    // 自定义运算符
-    @Bean
-    public MyOp myOp() {
-        return new MyOp();
     }
 
 }
--- bean-searcher-demos/spring-boot-demo/src/main/java/com/example/config/BeanSearcherConfig.java
+++ bean-searcher-demos/spring-boot-demo/src/main/java/com/example/config/BeanSearcherConfig.java
@@ -2,7 +2,9 @@ package com.example.config;
 
 import com.ejlchina.searcher.ParamFilter;
 import com.ejlchina.searcher.SqlExecutor;
+import com.ejlchina.searcher.boot.NamedDataSource;
 import com.ejlchina.searcher.implement.DefaultSqlExecutor;
+import com.example.operator.MyOp;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -15,61 +17,78 @@ import javax.sql.DataSource;
 @Configuration
 public class BeanSearcherConfig {
 
-    /**
-     * 为了简化多值参数传递
-     * 参考：https://github.com/ejlchina/bean-searcher/issues/10
-     * @return 参数过滤器
-     */
+    // 自定义运算符
     @Bean
-    public ParamFilter myParamFilter() {
-        return new JsonArrParamFilter();
+    public MyOp myOp() {
+        return new MyOp();
     }
 
     /**
-     * 以下是多数据源配置示例，可全部删除
+     * @return 慢 SQL 监听器
      */
-
-    @Bean(name = "primaryDsProps")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSourceProperties primaryDsProps() {
-        return new DataSourceProperties();
-    }
-
-    @Bean(name = "userDsProps")
-    @ConfigurationProperties(prefix = "spring.datasource.user")
-    public DataSourceProperties userDsProps() {
-        return new DataSourceProperties();
-    }
-
-    @Bean(name = "orderDsProps")
-    @ConfigurationProperties(prefix = "spring.datasource.order")
-    public DataSourceProperties orderDsProps() {
-        return new DataSourceProperties();
-    }
-
-    @Bean @Primary
-    public DataSource primaryDs(@Qualifier("primaryDsProps") DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean(name = "userDs")
-    public DataSource userDs(@Qualifier("userDsProps") DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean(name = "orderDs")
-    public DataSource orderDs(@Qualifier("orderDsProps") DataSourceProperties dataSourceProperties) {
-        return dataSourceProperties.initializeDataSourceBuilder().build();
-    }
-
     @Bean
-    public SqlExecutor sqlExecutor(DataSource dataSource,
-               @Qualifier("userDs") DataSource userDs,
-               @Qualifier("orderDs") DataSource orderDs) {
-        DefaultSqlExecutor executor = new DefaultSqlExecutor(dataSource);
-        executor.setDataSource("userDs", userDs);
-        executor.setDataSource("orderDs", orderDs);
-        return executor;
+    public SqlExecutor.SlowListener slowSqlListener() {
+        return (beanClass, slowSql, params, timeCost) -> {
+            System.out.println("慢 SQL 啦");
+        };
     }
+
+//
+//    /**
+//     * 为了简化多值参数传递
+//     * 参考：https://github.com/ejlchina/bean-searcher/issues/10
+//     * @return 参数过滤器
+//     */
+//    @Bean
+//    public ParamFilter myParamFilter() {
+//        return new JsonArrParamFilter();
+//    }
+//
+//    /**
+//     * 以下是多数据源配置示例，可全部删除
+//     */
+//
+//    @Bean(name = "primaryDsProps")
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSourceProperties primaryDsProps() {
+//        return new DataSourceProperties();
+//    }
+//
+//    @Bean(name = "userDsProps")
+//    @ConfigurationProperties(prefix = "spring.datasource.user")
+//    public DataSourceProperties userDsProps() {
+//        return new DataSourceProperties();
+//    }
+//
+//    @Bean(name = "orderDsProps")
+//    @ConfigurationProperties(prefix = "spring.datasource.order")
+//    public DataSourceProperties orderDsProps() {
+//        return new DataSourceProperties();
+//    }
+//
+//    @Bean @Primary
+//    public DataSource primaryDs(@Qualifier("primaryDsProps") DataSourceProperties dataSourceProperties) {
+//        return dataSourceProperties.initializeDataSourceBuilder().build();
+//    }
+//
+//    @Bean(name = "userDs")
+//    public DataSource userDs(@Qualifier("userDsProps") DataSourceProperties dataSourceProperties) {
+//        return dataSourceProperties.initializeDataSourceBuilder().build();
+//    }
+//
+//    @Bean(name = "orderDs")
+//    public DataSource orderDs(@Qualifier("orderDsProps") DataSourceProperties dataSourceProperties) {
+//        return dataSourceProperties.initializeDataSourceBuilder().build();
+//    }
+//
+//    @Bean
+//    public NamedDataSource userDataSource(@Qualifier("userDs") DataSource userDs) {
+//        return new NamedDataSource("userDs", userDs);
+//    }
+//
+//    @Bean
+//    public NamedDataSource orderDataSource(@Qualifier("orderDs") DataSource orderDs) {
+//        return new NamedDataSource("orderDs", orderDs);
+//    }
 
 }
