4b96a7f756fa3c46b31104db65849845a84c7d52
zhouxu <zhou.xu@ejlchina.com>
2022-06-17 20:00:28 +0800

    DateTimeParamConvertor

--- bean-searcher/src/main/java/com/ejlchina/searcher/convertor/DateTimeParamConvertor.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/convertor/DateTimeParamConvertor.java
@@ -6,14 +6,15 @@ import com.ejlchina.searcher.bean.DbType;
 import com.ejlchina.searcher.util.StringUtils;
 
 import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAccessor;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.TimeZone;
 import java.util.regex.Pattern;
+
+import static java.time.temporal.ChronoField.EPOCH_DAY;
 
 /**
  * [String | java.util.Date | LocalDate to java.sql.Date] 参数值转换器
@@ -26,6 +27,11 @@ public class DateTimeParamConvertor implements ParamResolver.Convertor {
     static final Pattern DATETIME_PATTERN = Pattern.compile("[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}");
 
     static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * 时区
+     */
+    private TimeZone timeZone = TimeZone.getDefault();
 
     @Override
     public boolean supports(DbType dbType, Class<?> valueType) {
@@ -42,25 +48,26 @@ public class DateTimeParamConvertor implements ParamResolver.Convertor {
             String datetime = normalize(s);
             if (DATETIME_PATTERN.matcher(datetime).matches()) {
                 TemporalAccessor accessor = FORMATTER.parse(datetime);
-                long days = accessor.getLong(ChronoField.EPOCH_DAY);
-                long mills = accessor.getLong(ChronoField.MILLI_OF_DAY);
-                return new Timestamp(TimeUnit.DAYS.toMillis(days) + mills);
+                LocalDateTime dateTime = LocalDate.ofEpochDay(accessor.getLong(EPOCH_DAY))
+                        .atTime(LocalTime.ofSecondOfDay(accessor.getLong(ChronoField.SECOND_OF_DAY)));
+                return toTimestamp(dateTime);
             }
         }
         if (value instanceof Date) {
             return new Timestamp(((Date) value).getTime());
         }
         if (value instanceof LocalDate) {
-            long days = ((LocalDate) value).getLong(ChronoField.EPOCH_DAY);
-            return new Timestamp(TimeUnit.DAYS.toMillis(days));
+            return toTimestamp(((LocalDate) value).atTime(0, 0, 0));
         }
         if (value instanceof LocalDateTime) {
-            LocalDateTime dateTime = (LocalDateTime) value;
-            long days = dateTime.getLong(ChronoField.EPOCH_DAY);
-            long mills = dateTime.getLong(ChronoField.MILLI_OF_DAY);
-            return new Timestamp(TimeUnit.DAYS.toMillis(days) + mills);
+            return toTimestamp((LocalDateTime) value);
         }
         return null;
+    }
+
+    private Timestamp toTimestamp(LocalDateTime dateTime) {
+        ZoneOffset offset = ZoneOffset.ofTotalSeconds(timeZone.getRawOffset() / 1000);
+        return new Timestamp(dateTime.toInstant(offset).toEpochMilli());
     }
 
     private String normalize(String datetime) {
@@ -75,6 +82,16 @@ public class DateTimeParamConvertor implements ParamResolver.Convertor {
             return datetime + " 00:00:00";
         }
         return datetime;
+    }
+
+    public ZoneId getZoneId() {
+        return timeZone.toZoneId();
+    }
+
+    public void setZoneId(ZoneId zoneId) {
+        if (zoneId != null) {
+            timeZone = TimeZone.getTimeZone(zoneId);
+        }
     }
 
 }
