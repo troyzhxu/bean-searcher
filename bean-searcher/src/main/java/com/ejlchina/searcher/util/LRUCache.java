0492e9fdab8ac4b09c8be29245b78c4f977f2a7e
zhouxu <zhou.xu@ejlchina.com>
2022-07-06 23:18:42 +0800

    优化 LRUCache，使其线程安全

--- bean-searcher/src/main/java/com/ejlchina/searcher/util/LRUCache.java
+++ bean-searcher/src/main/java/com/ejlchina/searcher/util/LRUCache.java
@@ -2,6 +2,8 @@ package com.ejlchina.searcher.util;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * LRU 缓存
@@ -12,6 +14,8 @@ import java.util.Map;
 public class LRUCache<T> extends LinkedHashMap<String, T> {
 
     private int maxCacheCount;
+
+    private final Lock lock = new ReentrantLock();
 
     public LRUCache(int maxCacheCount) {
         this.maxCacheCount = maxCacheCount;
@@ -28,6 +32,66 @@ public class LRUCache<T> extends LinkedHashMap<String, T> {
 
     public void setMaxCacheCount(int maxCacheCount) {
         this.maxCacheCount = maxCacheCount;
+    }
+
+    @Override
+    public T get(Object key) {
+        lock.lock();
+        try {
+            return super.get(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public T put(String key, T value) {
+        lock.lock();
+        try {
+            return super.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends T> m) {
+        lock.lock();
+        try {
+            super.putAll(m);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public T putIfAbsent(String key, T value) {
+        lock.lock();
+        try {
+            return super.putIfAbsent(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public boolean remove(Object key, Object value) {
+        lock.lock();
+        try {
+            return super.remove(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public T replace(String key, T value) {
+        lock.lock();
+        try {
+            return super.replace(key, value);
+        } finally {
+            lock.unlock();
+        }
     }
 
 }
