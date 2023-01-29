937c0cb9e7518ce9ba9b38179a4d03cfe0306ee2
troyzhxu <1573555987@qq.com>
2023-01-29 17:30:45 +0800

    MapUtils 新增 4 个 of(..) 便捷 Map 构造方法

--- bean-searcher/src/main/java/cn/zhxu/bs/util/MapUtils.java
+++ bean-searcher/src/main/java/cn/zhxu/bs/util/MapUtils.java
@@ -9,7 +9,7 @@ public class MapUtils {
 	/**
 	 * 将一个 value 为数组的 Map 对象，拉平为 value 为单值的 Map 对象
 	 * @param map 已有 Map 参数
-	 * @return Map 参数
+	 * @return Map 对象
 	 */
 	public static Map<String, Object> flat(Map<String, String[]> map) {
 		Map<String, Object> newMap = new HashMap<>();
@@ -46,6 +46,64 @@ public class MapUtils {
 	 */
 	public static MapBuilder builder(Map<String, Object> map) {
 		return new MapBuilder(map);
+	}
+
+	/**
+	 * 构建只有一个键值对的 Map 对象
+	 * @param key 键
+	 * @param value 值
+	 * @return Map 对象
+	 * @since v4.0.0
+	 */
+	public Map<String, Object> of(String key, Object value) {
+		Map<String, Object> map = new HashMap<>();
+		map.put(key, value);
+		return map;
+	}
+
+	/**
+	 * 构建只有两个键值对的 Map 对象
+	 * @param key1 第一个键
+	 * @param value1 第一个值
+	 * @param key2 第二个键
+	 * @param value2 第二个值
+	 * @return Map 对象
+	 * @since v4.0.0
+	 */
+	public Map<String, Object> of(String key1, Object value1, String key2, Object value2) {
+		Map<String, Object> map = new HashMap<>();
+		map.put(key1, value1);
+		map.put(key2, value2);
+		return map;
+	}
+
+	/**
+	 * 构建只有一个键值对的 Map 对象
+	 * @param key 键
+	 * @param value 值
+	 * @return Map 对象
+	 * @since v4.0.0
+	 */
+	public <T> Map<String, Object> of(FieldFns.FieldFn<T, ?> key, Object value) {
+		Map<String, Object> map = new HashMap<>();
+		map.put(FieldFns.name(key), value);
+		return map;
+	}
+
+	/**
+	 * 构建只有两个键值对的 Map 对象
+	 * @param key1 第一个键
+	 * @param value1 第一个值
+	 * @param key2 第二个键
+	 * @param value2 第二个值
+	 * @return Map 对象
+	 * @since v4.0.0
+	 */
+	public <T> Map<String, Object> of(FieldFns.FieldFn<T, ?> key1, Object value1, FieldFns.FieldFn<T, ?> key2, Object value2) {
+		Map<String, Object> map = new HashMap<>();
+		map.put(FieldFns.name(key1), value1);
+		map.put(FieldFns.name(key2), value2);
+		return map;
 	}
 
 }
