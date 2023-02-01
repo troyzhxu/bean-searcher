8ca6353659ccbe8db8c608d9e03ca45a4d5a9c2b
troyzhxu <1573555987@qq.com>
2023-02-01 17:11:34 +0800

    优化注释

--- bean-searcher/src/main/java/cn/zhxu/bs/convertor/JsonFieldConvertor.java
+++ bean-searcher/src/main/java/cn/zhxu/bs/convertor/JsonFieldConvertor.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * [字符串 to 数字] 字段转换器
+ * [Json 字符串 to 对象] 字段转换器
  * 与 {@link DefaultBeanReflector } 配合使用
  * @author Troy.Zhou @ 2021-11-01
  * @since v4.0.0
