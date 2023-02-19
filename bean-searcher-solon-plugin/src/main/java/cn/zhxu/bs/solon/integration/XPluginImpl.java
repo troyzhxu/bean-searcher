9693d7155da42c161549106a4dd3d99ba6372480
noear <noear@live.cn>
2023-02-19 17:35:58 +0800

    完善 bean-search-solon-plugin 适配。

--- bean-searcher-solon-plugin/src/main/java/cn/zhxu/bs/solon/integration/XPluginImpl.java
+++ bean-searcher-solon-plugin/src/main/java/cn/zhxu/bs/solon/integration/XPluginImpl.java
@@ -3,12 +3,12 @@ package cn.zhxu.bs.solon.integration;
 
 import cn.zhxu.bs.solon.BeanSearcherAutoConfiguration;
 import cn.zhxu.bs.solon.BeanSearcherProperties;
-import cn.zhxu.bs.solon.JsonFieldConvertorConfig;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
 
 /**
- * @author noear 2023/2/18 created
+ * @author noear
+ * @since 2.1
  */
 public class XPluginImpl implements Plugin {
     @Override
@@ -18,7 +18,6 @@ public class XPluginImpl implements Plugin {
         //容器加载完成后再执行，确保用户的 Bean 优先
         context.beanOnloaded(x -> {
             x.beanMake(BeanSearcherAutoConfiguration.class);
-            x.beanMake(JsonFieldConvertorConfig.class);
         });
     }
 }
