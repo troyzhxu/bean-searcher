8293d4a54f5f7739dc4427a10317d89c0e9a107e
noear <noear@live.cn>
2023-03-03 16:07:22 +0800

    solon 升为 2.2.1，并完善 bean-searcher-solon-plugin 适配

--- bean-searcher-demos/bs-demo-solon/src/main/java/com/example/demo/Config.java
+++ bean-searcher-demos/bs-demo-solon/src/main/java/com/example/demo/Config.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.noear.solon.core.util.ResourceUtil;
 import org.noear.wood.DbContext;
 import org.noear.wood.annotation.Db;
 
@@ -31,30 +32,9 @@ public class Config {
     }
 
     private void executeSqlFile(DbContext db, String sqlFilePath) throws IOException, SQLException {
-        String[] sqlList = readAsString(sqlFilePath).split(";");
+        String[] sqlList = ResourceUtil.getResourceAsString(sqlFilePath).split(";");
         for (String sql : sqlList) {
             db.sql(sql).execute();
         }
     }
-
-    public static String readAsString(String path) throws IOException {
-        InputStream input = Config.class.getClassLoader().getResourceAsStream(path);
-        if (input == null) {
-            throw new IllegalStateException("No resource found at " + path);
-        }
-        return readAsString(input);
-    }
-
-    public static String readAsString(InputStream input) throws IOException {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        try (input; output) {
-            byte[] buf = new byte[10240];
-            int len;
-            while ((len = input.read(buf)) != -1) {
-                output.write(buf, 0, len);
-            }
-        }
-        return output.toString();
-    }
-
 }
