70ef3b9447f8a19d829d9d99a479202528ae38ca
Troy <troy@kgu.cn>
2023-03-11 17:29:15 +0800

    fix: code format

--- bean-searcher/src/main/java/cn/zhxu/bs/util/MapUtils.java
+++ bean-searcher/src/main/java/cn/zhxu/bs/util/MapUtils.java
@@ -13,8 +13,8 @@ public class MapUtils {
 	 */
 	public static Map<String, Object> flat(Map<String, String[]> map) {
 		Map<String, Object> newMap = new HashMap<>();
-		for (Entry<String, String[]> entry: map.entrySet()) {
-			String[] values =entry.getValue();
+		for (Entry<String, String[]> entry : map.entrySet()) {
+			String[] values = entry.getValue();
 			if (values.length > 0) {
 				newMap.put(entry.getKey(), values[0]);
 			}
