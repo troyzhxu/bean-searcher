2f210ef80d6ba10438f612c8bcbfa448ae9bb345
Troy <troy@kgu.cn>
2023-03-11 23:10:40 +0800

    优化 SolonPlugin

--- bean-searcher-solon-plugin/src/main/java/cn/zhxu/bs/solon/SolonPlugin.java
+++ bean-searcher-solon-plugin/src/main/java/cn/zhxu/bs/solon/SolonPlugin.java
@@ -1,8 +1,5 @@
-package cn.zhxu.bs.solon.integration;
+package cn.zhxu.bs.solon;
 
-import cn.zhxu.bs.solon.ConfigurationAfter;
-import cn.zhxu.bs.solon.ConfigurationBefore;
-import cn.zhxu.bs.solon.BeanSearcherProperties;
 import org.noear.solon.core.AopContext;
 import org.noear.solon.core.Plugin;
 
@@ -10,14 +7,14 @@ import org.noear.solon.core.Plugin;
  * @author noear
  * @since 2.1
  */
-public class XPluginImpl implements Plugin {
+public class SolonPlugin implements Plugin {
+
     @Override
     public void start(AopContext context) {
         context.beanMake(BeanSearcherProperties.class);
 
         //容器加载完成后再执行，确保用户的 Bean 优先
-
-        //先处理没有 getBeansOfType 接口调用的
+        // 先处理没有 getBeansOfType 接口调用的
         context.lifecycle(-99, () -> {
             context.beanMake(ConfigurationBefore.class);
         });
