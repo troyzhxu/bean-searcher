7151a78fa023d4fb9e9bbc1c67bc3cdc242c1a18
Troy <troy@kgu.cn>
2023-03-11 16:40:21 +0800

    update demos

--- bean-searcher-demos/bs-demo-grails/src/main/groovy/com/demo/sbean/EmployeeBean.groovy
+++ bean-searcher-demos/bs-demo-grails/src/main/groovy/com/demo/sbean/EmployeeBean.groovy
@@ -5,23 +5,23 @@ import cn.zhxu.bs.bean.SearchBean
 import com.fasterxml.jackson.annotation.JsonFormat
 
 
-@SearchBean(tables = "employee e, department d", joinCond = "e.department_id = d.id")
+@SearchBean(
+    tables = "employee e, department d",
+    where = "e.department_id = d.id",
+    autoMapTo = "e"
+)
 class EmployeeBean {
 
-    @DbField("e.id")
     Long id;
 
-    @DbField("e.name")
     String name;
 
-    @DbField("e.age")
     Integer age;
 
     @DbField("d.name")
     String department;
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm", timezone = "GMT+8")
-    @DbField("e.entry_date")
     Date entryDate;
 
 }
