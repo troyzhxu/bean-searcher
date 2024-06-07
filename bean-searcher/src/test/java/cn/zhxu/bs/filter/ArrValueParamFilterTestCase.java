3b1b58c0ae49bfbfdf4c0b1bf7af2ec5b3c8d5de
troyzhxu <1573555987@qq.com>
2024-06-07 17:51:46 +0800

    test: ArrValueParamFilterTestCase

---
+++ bean-searcher/src/test/java/cn/zhxu/bs/filter/ArrValueParamFilterTestCase.java
@@ -0,0 +1,35 @@
+package cn.zhxu.bs.filter;
+
+import cn.zhxu.bs.util.MapUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrValueParamFilterTestCase {
+
+    ArrValueParamFilter filter = new ArrValueParamFilter();
+
+    @Test
+    public void test_01() {
+        Map<String, String[]> map = new HashMap<>();
+        map.put("id", new String[] { "1", "2", "3" });
+        map.put("id-op", new String[] { "il" });
+        map.put("name", new String[] { "Jack" });
+        map.put("age-bt", new String[] { "20", "30" });
+        map.put("sex", new String[] { "M", "F" });
+        Map<String, Object> params = filter.doFilter(null, MapUtils.flat(map));
+        Assertions.assertEquals(11, params.size());
+        Assertions.assertEquals("1", params.get("id-0"));
+        Assertions.assertEquals("2", params.get("id-1"));
+        Assertions.assertEquals("3", params.get("id-2"));
+        Assertions.assertEquals("il", params.get("id-op"));
+        Assertions.assertEquals("Jack", params.get("name"));
+        Assertions.assertArrayEquals(new Object[] { "20", "30" }, (Object[]) params.get("age-bt"));
+        Assertions.assertEquals("M", params.get("sex-0"));
+        Assertions.assertEquals("F", params.get("sex-1"));
+        System.out.println("\ttest_01 passed!");
+    }
+
+}
