19d6ad72c514cd871d89b1517258b1132b1d4f3e
Troy <troy@kgu.cn>
2024-08-23 00:43:07 +0800

    完善测试用例：JsonFieldConvertorTestCase

--- bean-searcher/src/test/java/cn/zhxu/bs/convertor/JsonFieldConvertorTestCase.java
+++ bean-searcher/src/test/java/cn/zhxu/bs/convertor/JsonFieldConvertorTestCase.java
@@ -1,5 +1,6 @@
 package cn.zhxu.bs.convertor;
 
+import cn.zhxu.bs.BeanMeta;
 import cn.zhxu.bs.FieldMeta;
 import cn.zhxu.bs.bean.DbField;
 import cn.zhxu.bs.bean.DbType;
@@ -7,14 +8,22 @@ import cn.zhxu.bs.implement.DefaultMetaResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class JsonFieldConvertorTestCase {
 
     final JsonFieldConvertor convertor = new JsonFieldConvertor();
 
-    final FieldMeta roleMeta = new DefaultMetaResolver().resolve(User.class).requireFieldMeta("role");
-    final FieldMeta rolesMeta = new DefaultMetaResolver().resolve(User.class).requireFieldMeta("roles");
+    final BeanMeta<User> beanMeta = new DefaultMetaResolver().resolve(User.class);
+
+    final FieldMeta roleMeta = beanMeta.requireFieldMeta("role");
+    final FieldMeta rolesMeta = beanMeta.requireFieldMeta("roles");
+    final FieldMeta kvsMeta = beanMeta.requireFieldMeta("kvs");
+    final FieldMeta kvListsMeta = beanMeta.requireFieldMeta("kvLists");
+
 
     static class User {
 
@@ -24,6 +33,41 @@ public class JsonFieldConvertorTestCase {
         @DbField(type = DbType.JSON)
         private Role role;
 
+        @DbField(type = DbType.JSON)
+        private List<KV<String, Integer>> kvs;
+
+        @DbField(type = DbType.JSON)
+        private List<List<KV<String, Integer>>> kvLists;
+
+    }
+
+    static class KV<K, T> {
+        private K k;
+        private T v;
+        public KV() {}
+        public KV(K k, T v) {
+            this.k = k;
+            this.v = v;
+        }
+        public K getK() {
+            return k;
+        }
+        public void setK(K k) {
+            this.k = k;
+        }
+        public T getV() {
+            return v;
+        }
+        public void setV(T v) {
+            this.v = v;
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            KV<?, ?> kv = (KV<?, ?>) o;
+            return Objects.equals(k, kv.k) && Objects.equals(v, kv.v);
+        }
     }
 
     static class Role {
@@ -48,6 +92,8 @@ public class JsonFieldConvertorTestCase {
     public void test_supports() {
         test_supports(roleMeta);
         test_supports(rolesMeta);
+        test_supports(kvsMeta);
+        test_supports(kvListsMeta);
         System.out.println("\ttest_support ok!");
     }
 
@@ -84,6 +130,43 @@ public class JsonFieldConvertorTestCase {
         Role role = (Role) data;
         Assertions.assertEquals(id, role.getId());
         Assertions.assertEquals(name, role.getName());
+    }
+
+    @Test
+    public void test_convert_kvs() {
+        String value = "[{\"k\":\"id\",\"v\":1},{\"k\":\"age\",\"v\":20}]";
+        Object results = convertor.convert(kvsMeta, value);
+        Assertions.assertNotNull(results);
+        Assertions.assertTrue(List.class.isAssignableFrom(results.getClass()));
+        assertKvList(results, Arrays.asList(
+                new KV<>("id", 1),
+                new KV<>("age", 20)
+        ));
+        System.out.println("\ttest_convert_kvs ok!");
+    }
+
+    private void assertKvList(Object results, List<KV<String, Integer>> expects) {
+        List<?> list = (List<?>) results;
+        Assertions.assertEquals(expects.size(), list.size());
+        for (int i = 0; i < expects.size(); i++) {
+            Assertions.assertEquals(expects.get(i), list.get(i));
+        }
+    }
+
+    @Test
+    public void test_convert_kvLists() {
+        String value = "[[{\"k\":\"id\",\"v\":1},{\"k\":\"age\",\"v\":20}],[{\"k\":\"idx\",\"v\":52}]]";
+        Object results = convertor.convert(kvListsMeta, value);
+        Assertions.assertNotNull(results);
+        Assertions.assertTrue(List.class.isAssignableFrom(results.getClass()));
+        List<?> list = (List<?>) results;
+        Assertions.assertEquals(2, list.size());
+        assertKvList(list.get(0), Arrays.asList(
+                new KV<>("id", 1),
+                new KV<>("age", 20)
+        ));
+        assertKvList(list.get(1), Collections.singletonList(new KV<>("idx", 52)));
+        System.out.println("\ttest_convert_kvLists ok!");
     }
 
 }
