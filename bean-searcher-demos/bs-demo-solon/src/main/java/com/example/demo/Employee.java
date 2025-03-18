6c2716bd6b9ea1c0794d4fccfdee04e413f23b7e
troyzhxu <1573555987@qq.com>
2025-03-18 18:58:58 +0800

    测试 bean-searcher-label

--- bean-searcher-demos/bs-demo-solon/src/main/java/com/example/demo/Employee.java
+++ bean-searcher-demos/bs-demo-solon/src/main/java/com/example/demo/Employee.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import cn.zhxu.bs.bean.DbField;
 import cn.zhxu.bs.bean.SearchBean;
+import cn.zhxu.bs.label.LabelFor;
 import cn.zhxu.bs.operator.Contain;
 import cn.zhxu.bs.operator.StartWith;
 import org.noear.snack.annotation.ONodeAttr;
@@ -24,12 +25,14 @@ public class Employee {
 
     private Gender gender;
 
+    @LabelFor("gender")
+    private String genderName;
+
     @DbField("d.name")
     private String department;
 
     @ONodeAttr(format = "yyyy-MM-dd HH:mm")
     private LocalDateTime entryDate;
-
 
     public int getId() {
         return id;
@@ -45,6 +48,10 @@ public class Employee {
 
     public Gender getGender() {
         return gender;
+    }
+
+    public String getGenderName() {
+        return genderName;
     }
 
     public String getDepartment() {
