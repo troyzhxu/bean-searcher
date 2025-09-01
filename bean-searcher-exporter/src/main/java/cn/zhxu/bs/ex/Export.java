6f198a1dcfb493e538d84a013391134d25695bff
troyzhxu <1573555987@qq.com>
2025-09-01 15:05:50 +0800

    docs(bs-exporter): 完善 Export 注解文档 - 对 expr() 和 format() 方法的说明进行了详细补充 - 增加了对不同环境下表达式语法的解释 - 提出了自定义表达式解析和格式化规则的建议

--- bean-searcher-exporter/src/main/java/cn/zhxu/bs/ex/Export.java
+++ bean-searcher-exporter/src/main/java/cn/zhxu/bs/ex/Export.java
@@ -23,13 +23,18 @@ public @interface Export {
     int idx() default 0;
 
     /**
-     * 支持 SpEL 表达式（Spring 环境下）或 SnEL 表达式（Solon 环境下），使用 @ 符引用当前字段的值，使用字段名引用其它字段的值
+     * 值转换表达式，用与在导出前将字段值进行变换，可直接使用 @ 符引用当前字段的值，使用字段名引用其它字段的值 <br>
+     * 在 Spring 环境下支持 SpEL 表达式语法 <br>
+     * 在 Solon 环境下支持 SnEL 表达式语法 <br>
+     * 你也可以通过自定义 {@link Expresser } 来重新定义表达式的解析规则
      * @return 值转换表达式
      */
     String expr() default "";
 
     /**
-     * 支持 日期 / 数字 / 字符串 格式
+     * 格式化输出，支持 日期 / 数字 / 字符串 三种类型的格式 <br>
+     * 如果同时配置了值转换表达式 {@link #expr()}, 则先进行表达式值转换，然后再对表达式的计算结果进行格式化 <br>
+     * 你也可以通过自定义 {@link Formatter } 来重新定义格式化的规则
      * @return 格式
      */
     String format() default "";
