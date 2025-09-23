98ed051f65098218c7352aa48e5692284b9dc0bf
troyzhxu <1573555987@qq.com>
2025-09-23 16:38:08 +0800

    feat(bean-searcher): 扩展字符串字段转换器以支持日期类型新增对 LocalDate、LocalDateTime 以及 java.util.Date 等日期类型的自动转换支持，使其能够正确地将这些类型转换为字符串形式。

--- bean-searcher/src/main/java/cn/zhxu/bs/convertor/StringFieldConvertor.java
+++ bean-searcher/src/main/java/cn/zhxu/bs/convertor/StringFieldConvertor.java
@@ -6,9 +6,13 @@ import cn.zhxu.bs.SearchException;
 
 import java.sql.Clob;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
- * [Clob | Number | Boolean to String] 字段转换器
+ * [Clob | Number | Boolean | Date to String] 字段转换器
  * @author Troy.Zhou @ 2025-09-22
  * @since v4.6.0
  */
@@ -17,7 +21,12 @@ public class StringFieldConvertor implements FieldConvertor.BFieldConvertor {
     @Override
     public boolean supports(FieldMeta meta, Class<?> valueType) {
         if (meta.getType() == String.class) {
-            return Clob.class.isAssignableFrom(valueType) || Number.class.isAssignableFrom(valueType) || valueType == Boolean.class;
+            return Clob.class.isAssignableFrom(valueType)
+                    || valueType == LocalDate.class
+                    || valueType == LocalDateTime.class
+                    || Date.class.isAssignableFrom(valueType)
+                    || Number.class.isAssignableFrom(valueType)
+                    || valueType == Boolean.class;
         }
         return false;
     }
@@ -26,6 +35,22 @@ public class StringFieldConvertor implements FieldConvertor.BFieldConvertor {
     public Object convert(FieldMeta meta, Object value) {
         if (value instanceof Clob) {
             return fromClob((Clob) value);
+        }
+        if (value instanceof LocalDate) {
+            return ((LocalDate) value).toString();
+        }
+        if (value instanceof LocalDateTime) {
+            return ((LocalDateTime) value).toString();
+        }
+        if (value instanceof java.sql.Date) {
+            return ((java.sql.Date) value).toLocalDate().toString();
+        }
+        if (value instanceof java.sql.Time) {
+            return ((java.sql.Time) value).toLocalTime().toString();
+        }
+        if (value instanceof Date) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return sdf.format((Date) value);
         }
         return value.toString();
     }
