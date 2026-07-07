955155170149c2a9e413aeaf8cff37e4e0b9f617
troyzhxu <1573555987@qq.com>
2026-07-07 16:32:22 +0800

    v4.8.8

--- bean-searcher-doc/docs/.vitepress/config/en.ts
+++ bean-searcher-doc/docs/.vitepress/config/en.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     nav: [
       { text: 'Guide', link: '/en/guide/info/bean-searcher', activeMatch: '/guide/' },
       {
-        text: 'v4.8.7',
+        text: 'v4.8.8',
         items: [
           {
             text: 'Changelog',
--- bean-searcher-doc/docs/.vitepress/config/zh.ts
+++ bean-searcher-doc/docs/.vitepress/config/zh.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     nav: [
       { text: '指南', link: '/guide/info/bean-searcher', activeMatch: '/guide/' },
       {
-        text: 'v4.8.7',
+        text: 'v4.8.8',
         items: [
           {
             text: '更新日志',
